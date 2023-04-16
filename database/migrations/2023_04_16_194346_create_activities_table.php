89109967e3795eac99f5e936bcf8c87930c742f8
AaEzha <aaezha@gmail.com>
2023-04-17 02:58:37 +0700

    feat(activity): model init 🔥

---
+++ database/migrations/2023_04_16_194346_create_activities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Activity;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('slug')->nullable();
+            $table->text('description')->nullable();
+            $table->string('logo')->nullable();
+            $table->text('logo_description')->nullable();
+            $table->string('status', 15)->default(Activity::STATUS_ACTIVE);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities');
+    }
+};
