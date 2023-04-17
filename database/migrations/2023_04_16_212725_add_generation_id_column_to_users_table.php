66e1d222b5931c292864d2f2dc7fb2baa0a88230
AaEzha <aaezha@gmail.com>
2023-04-18 04:49:00 +0700

    db(users): add foreign id for generation 🔥

---
+++ database/migrations/2023_04_16_212725_add_generation_id_column_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Generation;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignIdFor(Generation::class)->nullable()->after('password')->constrained()->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeignIdFor(Generation::class);
+            $table->dropColumn('generation_id');
+        });
+    }
+};
